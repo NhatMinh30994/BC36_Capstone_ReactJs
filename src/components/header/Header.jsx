8153090738a248e9df1d1d97d2bb8286b4bb53c8
hung <vumanhhung27092018@gmail.com>
2023-03-01 23:58:51 +0700

    xog respon

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -17,8 +17,7 @@ export default function Header() {
 
   return (
     <div>
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.csss" />
-      <nav className="navbar navbar-expand-sm dark">
+      <nav className="navbar navbar-expand-md dark">
         <button
           className="navbar-toggler d-lg-none"
           type="button"
@@ -28,39 +27,52 @@ export default function Header() {
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
-          <span>
-            <i
-              className="pl-3 bg-secondary pr-3 fa-solid fa-bars"
-              aria-hidden="true"
-            />
+          <span className="text-dark rounded bg-white  pl-2 pr-2 pb-1">
+            <svg className="text-center"
+              xmlns="http://www.w3.org/2000/svg"
+              width="25"
+              height="25"
+              fill="currentColor"
+              class="bi bi-card-text"
+              viewBox="0 0 16 16"
+            >
+              <path d="M14.5 3a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-13a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h13zm-13-1A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h13a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-13z" />
+              <path d="M3 5.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3 8a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9A.5.5 0 0 1 3 8zm0 2.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5z" />
+            </svg>
           </span>
         </button>
         <div className="collapse navbar-collapse" id="collapsibleNavId">
-          <ul className="navbar-nav mr-auto mt-2 mt-lg-0  flex-wrap">
-            <li className="nav-item active li">
-              <NavLink className="prl" to="/">
+          <ul className="navbar-nav mr-auto mt-2 mt-lg-0 text-center">
+            <li className="nav-item active li" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <NavLink className="prl nav-link" to="/">
                 Home
               </NavLink>
-              <a href="#phim" className="prl" to="/movie-list">
+            </li>
+            <li className="nav-item li" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <a href="#phim" className="prl nav-link" to="/movie-list">
                 Phim
               </a>
-              <a href="#tintuc" className="prl" to="/">
+            </li>
+            <li className="nav-item li" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <a href="#tintuc" className="prl nav-link">
                 Tin Tức
               </a>
-              <a href="#sukien" className="prl" to="/">
+            </li>
+            <li className="nav-item li" data-toggle="collapse" data-target=".navbar-collapse.show">
+              <a href="#sukien" className="prl nav-link">
                 Sự Kiện
               </a>
             </li>
           </ul>
-          <div className="ml-auto">
+          <div className="ml-auto text-center">
             {userState.userInfo ? (
               <>
-                <span className="mr-3 text-danger ">
-                  {/* Hello {userState.userInfo.hoTen} */}
-                </span>
+                {/* <span className="mr-3 text-danger ">
+                  Hello {userState.userInfo.hoTen}
+                </span> */}
                 <button
                   onClick={handleLogout}
-                  className="btn btn-outline-danger font-weight-bold rounded-pill border-0 "
+                  className="btn btn-outline-danger font-weight-bold rounded-pill border-0"
                 >
                   LOGOUT
                 </button>
@@ -76,7 +88,7 @@ export default function Header() {
                 </button>
                 <button
                   onClick={() => navigate("/login")}
-                  className="btn btn-outline-success my-2 my-sm-0  rounded-pill border-0"
+                  className="btn btn-outline-success my-2 my-sm-0 mr-4 rounded-pill border-0"
                 >
                   LOGIN
                 </button>
