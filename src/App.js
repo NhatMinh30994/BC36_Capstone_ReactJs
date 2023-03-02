f9c38797865fe7e7801fbc37d9132798672669a5
hung <vumanhhung27092018@gmail.com>
2023-03-03 00:17:25 +0700

    add backtotop

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import Router from "./routes/Router";
 import { LoadingProvider } from "./contexts/loading/LoadingContext";
+import BackToTopButton from "components/BackToTopButton";
 
 function App() {
   return (
     <BrowserRouter>
       <LoadingProvider>
         <Router />
+        <BackToTopButton />
       </LoadingProvider>
     </BrowserRouter>
   );
